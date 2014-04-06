f902ca033522de3e5493c452f12dc2fec7d8c492
Tihomir Yanev <tisho91@gmail.com>
2014-04-06 19:35:28 +0300

    Created two dynamic class and wrote more tests

--- core/piece.py
+++ core/piece.py
@@ -1,13 +1,14 @@
 import settings
 
 class Piece:
-    def __init__(self, name, label, points, directions):
-        self.name = name
-        self.label = label
-        self.points = points
-        self.directions = directions
+    #    self.label = label
+    #    self.points = points
+    #    self.directions = directions
+    #    self.color = color
+    #    self.moves = 0
 
-pieces = {'King': {'points': 1000, 'directions': []}}
+    def is_alias(piece):
+        return self.color == piece.color
 
 class Square:
     def __init__(self, index):
@@ -26,5 +27,47 @@ class Board():
     def __init__(self):
         self.squares = [Square(i) for i in range(settings.BOARD_SIZE ** 2)]
 
-    def __getitem__(self, item):
-        return ''
+    def place(self, piece, notation):
+        self[self.notation_to_id(notation)] = piece
+
+    def notation_to_id(self, notation):
+        return settings.Y_LABELS.index(notation[0]) + \
+            (int(notation[1]) - 1) * settings.BOARD_SIZE
+
+    def __getitem__(self, index):
+        return self.squares[index]
+
+    def __setitem__(self, index, item):
+        self.squares[index].piece = item
+
+ROOK_MOVE = [1, 8]
+
+BISHOP_MOVE = [7, 9]
+
+KING_MOVE = ROOK_MOVE + BISHOP_MOVE
+
+
+PIECES = {'King': dict(label='K', points=1000, long_move=False,
+                    moves=[KING_MOVE], capture_moves=[],
+                    special_moves=[], start_pos = ['e1', 'e8']),
+          'Queen': dict(label='Q', points=9, long_move=True,
+                    moves=[], capture_moves=[], special_moves=[],
+                    start_pos = ['d1', 'd8'])
+         }
+
+CHESS_SET = dict((cls, type(cls, (Piece,), options)) \
+    for cls, options in PIECES.items())
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.reset()
+
+    def reset(self):
+        for piece in CHESS_SET.values():
+            for square in piece.start_pos:
+                self.board.place(piece, square)
+
+    def move(square1, square2):
+        self.board[square2].piece = self.board[square1].piece
+        self.board[square1] = None
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -3,7 +3,26 @@ import sys
 sys.path.append('core')
 
 import piece
-board  = piece.Board()
+board = piece.Board()
+
+class KingTest(unittest.TestCase):
+    def setUp(self):
+        self.king = piece.CHESS_SET['King']
+
+    def test_points(self):
+        self.assertEqual(1000, self.king.points)
+
+class GameTest(unittest.TestCase):
+    def setUp(self):
+        self.game = piece.Game()
+        self.white_king = self.game.board[4].piece
+
+    def test_king_location(self):
+        self.assertIs(self.white_king,
+            piece.CHESS_SET['King'])
+
+    #def test_king_color(self):
+    #   self.assertEqual(self.white_king.color, 0)
 
 class BoardTest(unittest.TestCase):
     def setUp(self):
@@ -12,14 +31,21 @@ class BoardTest(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.y, len(board.squares))
 
+    @unittest.skip('not implemented')
     def test_square_colors(self):
-        self.assertEqual(0, board.squares[8].color)
+        pass#self.assertEqual(0, board.squares[8].color)
+
+    def test_square_indexing(self):
+        self.assertIs(board[2], board.squares[2])
 
     def test_square_name(self):
         self.assertEqual('b1', str(board.squares[1]))
 
     def test_square_occupation(self):
         self.assertFalse(board.squares[3].is_occupied())
+
+    def test_notation_to_id(self):
+        self.assertEqual(board.notation_to_id('e1'), 4)
 
 if __name__ == '__main__':
     unittest.main()
