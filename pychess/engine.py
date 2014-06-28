68bc455ba009827539e7c17148ee60d8c18c0a0f
Tihomir Yanev <tisho91@gmail.com>
2014-06-28 13:49:44 +0300

    Fixed engine promotion bug

--- pychess/engine.py
+++ pychess/engine.py
@@ -6,4 +6,6 @@ class Engine:
     def get_reply(self, fen, strength):
         output = str(subprocess.check_output(["sh", "bin/stockfish.sh", '{}'.format(fen), str(strength)]))
         move = re.search('bestmove (.+?) ', output).group(1)
+        if len(move) > 4:
+            move = move[:4] + '=' + move[-1].upper()
         return move[:2] + '-' + move[2:]
